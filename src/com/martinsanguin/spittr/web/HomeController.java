c88b6a0ab4be9ca5596e04e2a9ada31dc673ad76
Martin Sanguin <martinsanguin@MacBook-Pro-de-Martin.local>
2019-10-11 12:20:11 -0300

    First commit, project configuration + first controller

---
+++ src/com/martinsanguin/spittr/web/HomeController.java
@@ -0,0 +1,14 @@
+package com.martinsanguin.spittr.web;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+	@RequestMapping(method=GET)
+	public String home() {
+		return "home";
+	}
+}
